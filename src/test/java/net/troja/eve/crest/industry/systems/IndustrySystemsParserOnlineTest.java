5f073eff1d2a3fa1c3cd7a739b3c4c30eae26e8f
Jens Oberender <j.obi@troja.net>
2015-01-29 07:39:43 +0100

    Just some renaming of two tests and clearify a comment.

--- src/test/java/net/troja/eve/crest/industry/systems/IndustrySystemsParserOnlineTest.java
+++ src/test/java/net/troja/eve/crest/industry/systems/IndustrySystemsParserOnlineTest.java
@@ -2,17 +2,22 @@ package net.troja.eve.crest.industry.systems;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.IOException;
+
 import org.junit.Test;
 
-public class IndustrySystemsParseTest {
-    public static final String LIVE_DATA = "src/test/resources/IndustrySystems.json";
-
+public class IndustrySystemsParserOnlineTest {
     @Test
     public void testGetData() {
 	final IndustrySystemsParser parser = new IndustrySystemsParser();
-	parser.setDummyDataFile(LIVE_DATA);
 	final IndustrySystems data = parser.getData();
 	assertEquals(5431, data.getTotalCount());
 	assertEquals(data.getTotalCount(), data.getItems().size());
+
+	try {
+	    parser.writeRawData(IndustrySystemsParserTest.LIVE_DATA);
+	} catch (final IOException e) {
+	    e.printStackTrace();
+	}
     }
 }
--- src/test/java/net/troja/eve/crest/industry/systems/IndustrySystemsParserTest.java
+++ src/test/java/net/troja/eve/crest/industry/systems/IndustrySystemsParserTest.java
@@ -2,22 +2,17 @@ package net.troja.eve.crest.industry.systems;
 
 import static org.junit.Assert.assertEquals;
 
-import java.io.IOException;
-
 import org.junit.Test;
 
-public class IndustrySystemsParseOnlineTest {
+public class IndustrySystemsParserTest {
+    public static final String LIVE_DATA = "src/test/resources/IndustrySystems.json";
+
     @Test
     public void testGetData() {
 	final IndustrySystemsParser parser = new IndustrySystemsParser();
+	parser.setDummyDataFile(LIVE_DATA);
 	final IndustrySystems data = parser.getData();
 	assertEquals(5431, data.getTotalCount());
 	assertEquals(data.getTotalCount(), data.getItems().size());
-
-	try {
-	    parser.writeRawData(IndustrySystemsParseTest.LIVE_DATA);
-	} catch (final IOException e) {
-	    e.printStackTrace();
-	}
     }
 }
