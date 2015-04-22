935250a266488bc190b8d1ac72beff4a02c018f5
Jens Oberender <j.obi@troja.net>
2015-04-22 23:32:12 +0200

    Changed to a release version number and added constants for the industry activities.

---
+++ src/main/java/net/troja/eve/crest/industry/systems/IndustryActivitys.java
@@ -0,0 +1,10 @@
+package net.troja.eve.crest.industry.systems;
+
+public interface IndustryActivitys {
+    int Manufacturing = 1;
+    int ResearchingTimeEfficiency = 3;
+    int ResearchingMaterialEfficiency = 4;
+    int Copying = 5;
+    int ReverseEngineering = 7;
+    int Invention = 8;
+}
--- src/main/java/net/troja/eve/crest/market/prices/MarketPricesParser.java
+++ src/main/java/net/troja/eve/crest/market/prices/MarketPricesParser.java
@@ -1,5 +1,8 @@
 package net.troja.eve.crest.market.prices;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import net.troja.eve.crest.PublicContainerParser;
 
 public class MarketPricesParser extends PublicContainerParser<MarketPrices, MarketPrice> {
@@ -10,5 +13,16 @@ public class MarketPricesParser extends PublicContainerParser<MarketPrices, Mark
     @Override
     protected String getPath() {
 	return "/market/prices/";
+    }
+    
+    public Map<Integer, Double> getDataAsMap() {
+	final MarketPrices data = getData();
+	final Map<Integer, Double> map = new HashMap<>();
+	if (data == null)
+	    return map;
+	for(final MarketPrice price : data.getItems()) {
+	    map.put(price.getType().getId(), price.getAdjustedPrice());
+	}
+	return map;
     }
 }
